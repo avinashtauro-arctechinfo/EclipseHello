d623dc8f682c4afebc8e578acc539fd9336a4978
Avinash Tauro <59994861+avinashtauro-arctechinfo@users.noreply.github.com>
2022-03-22 08:25:25 +0530

    Update Program.java
    Added some changes

--- src/Program.java
+++ src/Program.java
@@ -3,5 +3,10 @@ public class Program
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World4");
+		
+		for(int i = 1; i <= 10; i++)
+		{
+			System.out.println(i + ".Test");
+		}
 	}
 }
